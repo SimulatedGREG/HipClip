ae56abbcc34dbec3142d108981b915597d7c817f
SimulatedGREG <simulatedgreg@gmail.com>
2015-12-21 01:19:17 -0600

    more logic for basic items, using localstorage

--- app.js
+++ app.js
@@ -4,52 +4,83 @@ var HipClip = Object.create({}),
   _Storage = Object.create({});
 
 HipClip._init = function() {
-  //clean storage for expired items
-  //load existing items
-  //start watcher
+  _Storage.clean();
+
+  this.populate();
+  this.watch();
 };
 
 HipClip.watch = function() {
-  //watch clipboard
+  var watcher = setInterval(function() {
+    if(this.readBoard) this.captureBoard();
+  }, 200);
 };
 
 HipClip.readBoard = function() {
-  //if new copy
+  //if new item
     //if text/image
-      //captureboard
+      // return true;
+    //else
+      // return false;
 };
 
 HipClip.captureBoard = function() {
   //capture clipboard item
   //pass data & type to _Storage
+  HipClip.populate();
 };
 
 HipClip.populate = function() {
-  //get all items from localStorage
+  var items = _Storage.getAll();
   //render items to DOM
 };
 
 _Storage.new = function(data, type) {
-  //store item in localStorage {id,timestamp,data,type}
+  var id = this.genId();
+  localStorage.setItem(id, JSON.stringify({
+    data: data,
+    type: type,
+    timestamp: Date.now(),
+    id: id
+  }));
 };
 
 _Storage.remove = function(id) {
-  //remove item by id
+  localStorage.removeItem(id);
 };
 
 _Storage.get = function(id) {
-  //return item by id
+  return localStorage.getItem(id);
 };
 
 _Storage.getAll = function() {
-  //return all items in localStorage
+  var items = [];
+  for(var i = 0; i < localStorage.length; i++) {
+    try {
+      items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+    } catch(err) {}
+  }
+  return items;
 };
 
 _Storage.getMostRecentItem = function() {
-  //return most recent item
+  return JSON.parse(
+    localStorage.getItem(localStorage.key(localStorage.length-1))
+  );
 };
 
 _Storage.clean = function() {
   //get current time
   //loop through localStorage and remove expired items
 };
+
+_Storage.genId = function() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4();
+};
+
+HipClip._init();
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict';
 const electron = require('electron');
-const clipboard = electron.clipboard;
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
