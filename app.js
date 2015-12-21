bb5d90cd4b4f4c9f968c29cdf5934ef283eee4b5
SimulatedGREG <simulatedgreg@gmail.com>
2015-12-21 05:47:58 -0600

    localstorage is simple&easy, but now i see its flaws

--- app.js
+++ app.js
@@ -10,23 +10,28 @@ HipClip._init = function() {
   this.watch();
 };
 
+
+// TODO: figure out this this
 HipClip.watch = function() {
   var watcher = setInterval(function() {
-    if(this.readBoard) this.captureBoard();
+    HipClip.readBoard();
   }, 200);
 };
 
+// TODO: support html format eventually
 HipClip.readBoard = function() {
-  //if new item
-    //if text/image
-      // return true;
-    //else
-      // return false;
+  if(clipboard.availableFormats().length > 0) {
+    var format = clipboard.availableFormats()[0].split('/')[0],
+      item = (format == 'text') ? clipboard.readText() : clipboard.readImage().toDataUrl();
+    if(_Storage.getLastItem() === null || item != _Storage.getLastItem())
+      this.captureBoard(item, format);
+  }
 };
 
-HipClip.captureBoard = function() {
-  //capture clipboard item
-  //pass data & type to _Storage
+HipClip.captureBoard = function(data, format) {
+  if(format == 'image')
+    data = clipboard.readImage().toDataUrl();
+  _Storage.new(data, format);
   HipClip.populate();
 };
 
@@ -35,14 +40,9 @@ HipClip.populate = function() {
   //render items to DOM
 };
 
-_Storage.new = function(data, type) {
-  var id = this.genId();
-  localStorage.setItem(id, JSON.stringify({
-    data: data,
-    type: type,
-    timestamp: Date.now(),
-    id: id
-  }));
+_Storage.new = function(data, format) {
+  var t = Date.now();
+  localStorage.setItem(t, data);
 };
 
 _Storage.remove = function(id) {
@@ -55,7 +55,7 @@ _Storage.get = function(id) {
 
 _Storage.getAll = function() {
   var items = [];
-  for(var i = 0; i < localStorage.length; i++) {
+  for(var i = 0, len = localStorage.length; i < len; i++) {
     try {
       items.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
     } catch(err) {}
@@ -63,10 +63,9 @@ _Storage.getAll = function() {
   return items;
 };
 
-_Storage.getMostRecentItem = function() {
-  return JSON.parse(
-    localStorage.getItem(localStorage.key(localStorage.length-1))
-  );
+_Storage.getLastItem = function() {
+  var len = localStorage.length;
+  return (len > 0) ? localStorage.getItem(localStorage.key(len-1)) : null;
 };
 
 _Storage.clean = function() {
@@ -81,6 +80,10 @@ _Storage.genId = function() {
       .substring(1);
   }
   return s4() + s4();
+};
+
+_Storage.removeAll = function() {
+  localStorage.clear();
 };
 
 HipClip._init();
